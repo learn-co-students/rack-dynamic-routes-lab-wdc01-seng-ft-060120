8b877597e4e0dbd4df74ef11a54cd535d6a2a7a2
Marcos Velasco <devslife7@gmail.com>
2020-06-21 20:45:34 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,29 @@
+class Application
+    # attr_accessor :items
+
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        # @@items = []
+
+        if req.path.match(/items/)
+            
+            item_name = req.path.split("/items/").last #turn /items/Figs into Figs
+            item = @@items.find{|s| s.name == item_name}
+
+            if @@items.include?(item)
+                resp.write item.price
+                resp.status = 200
+            else
+                resp.write "Item not found"
+                resp.status = 400
+            end
+        else
+            resp.write "Route not found"
+            resp.status = 404
+        end
+
+        resp.finish
+    end
+end
